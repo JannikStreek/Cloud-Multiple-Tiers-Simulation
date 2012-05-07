4c21993c5e9782d86462e6ad0cbeae40ce311953
Christoph Müller <christoph.t.mueller@gmail.com>
2012-05-07 21:23:48 +0200

    Starting off with cloudSim ex. 1, reimplementing for better structure. Running example.

---
+++ CloudSim/src/de/hpi_web/cloudSim/staticTier/DatacenterFactory.java
@@ -0,0 +1,79 @@
+package de.hpi_web.cloudSim.staticTier;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.cloudbus.cloudsim.Datacenter;
+import org.cloudbus.cloudsim.DatacenterCharacteristics;
+import org.cloudbus.cloudsim.Host;
+import org.cloudbus.cloudsim.Pe;
+import org.cloudbus.cloudsim.Storage;
+import org.cloudbus.cloudsim.VmAllocationPolicySimple;
+import org.cloudbus.cloudsim.VmSchedulerTimeShared;
+import org.cloudbus.cloudsim.provisioners.BwProvisionerSimple;
+import org.cloudbus.cloudsim.provisioners.PeProvisionerSimple;
+import org.cloudbus.cloudsim.provisioners.RamProvisionerSimple;
+
+public class DatacenterFactory {
+	
+	/**
+	 * Creates the datacenter.
+	 *
+	 * @param name the name
+	 *
+	 * @return the datacenter
+	 */
+	public static Datacenter createDatacenter(String name) {
+		List<Host> hostList = new ArrayList<Host>();
+		List<Pe> peList = new ArrayList<Pe>();
+
+		int mips = 1000;
+		peList.add(new Pe(0, new PeProvisionerSimple(mips)));
+
+		hostList.add(defaultHost(0, peList));
+		return createDatacenter(name, defaultCharacteristics(hostList), hostList);
+	}
+	public static Datacenter createDatacenter(String name, DatacenterCharacteristics chars, List<Host> hostList) {
+
+		LinkedList<Storage> storageList = new LinkedList<Storage>(); // we are not adding SAN
+		Datacenter datacenter = null;
+		try {
+			datacenter = new Datacenter(name, chars, new VmAllocationPolicySimple(hostList), storageList, 0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return datacenter;
+	}
+	
+	private static DatacenterCharacteristics defaultCharacteristics(List<Host> hostList) {
+		String arch = "x86"; // system architecture
+		String os = "Linux"; // operating system
+		String vmm = "Xen";
+		double time_zone = 10.0; // time zone this resource located
+		double cost = 3.0; // the cost of using processing in this resource
+		double costPerMem = 0.05; // the cost of using memory in this resource
+		double costPerStorage = 0.001; // the cost of using storage in this
+										// resource
+		double costPerBw = 0.0; // the cost of using bw in this resource
+
+		return new DatacenterCharacteristics(
+				arch, os, vmm, hostList, time_zone, cost, costPerMem,
+				costPerStorage, costPerBw);
+	}
+	
+	private static Host defaultHost(int hostId, List<Pe> peList) {
+		int ram = 2048;
+		long storage = 1000000;
+		int bw = 10000;
+		return new Host(
+				hostId,
+				new RamProvisionerSimple(ram),
+				new BwProvisionerSimple(bw),
+				storage,
+				peList,
+				new VmSchedulerTimeShared(peList)
+			);
+	}
+}
---
+++ CloudSim/src/de/hpi_web/cloudSim/staticTier/StaticTier.java
@@ -0,0 +1,87 @@
+package de.hpi_web.cloudSim.staticTier;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import org.cloudbus.cloudsim.Cloudlet;
+import org.cloudbus.cloudsim.Datacenter;
+import org.cloudbus.cloudsim.DatacenterBroker;
+import org.cloudbus.cloudsim.Log;
+import org.cloudbus.cloudsim.Vm;
+import org.cloudbus.cloudsim.core.CloudSim;
+
+import de.hpi_web.cloudSim.staticTier.DatacenterFactory;
+import de.hpi_web.cloudSim.utils.OutputWriter;
+
+public class StaticTier {
+
+
+	/** The cloudlet list. */
+	private static List<Cloudlet> cloudletList;
+
+	/** The vmlist. */
+	private static List<Vm> vmlist;
+	
+	public static void main(String[] args) {
+
+		Log.printLine("Starting StaticTier...");
+		initializeCloudSim();
+		Datacenter datacenter0 = DatacenterFactory.createDatacenter("Datacenter_0");
+		DatacenterBroker broker = createBroker();
+		Vm vm = VmFactory.createVm(broker.getId());
+		vmlist = new ArrayList<Vm>();
+		vmlist.add(vm);
+
+		// submit vm list to the broker
+		broker.submitVmList(vmlist);
+
+		// Create Cloudlet(s)
+		cloudletList = new ArrayList<Cloudlet>();
+		Cloudlet cloudlet = CloudletFactory.createCloudlet(broker.getId(), vm.getId());
+		cloudletList.add(cloudlet);
+
+		// submit cloudlet list to the broker
+		broker.submitCloudletList(cloudletList);
+
+		CloudSim.startSimulation();
+		CloudSim.stopSimulation();
+
+		//Print results
+		List<Cloudlet> newList = broker.getCloudletReceivedList();
+		OutputWriter.printCloudletList(newList);
+
+		// Print the debt of each user to each datacenter
+		datacenter0.printDebts();
+
+		Log.printLine("CloudSimExample1 finished!");
+		
+	}
+	
+	public static void initializeCloudSim() {
+		int num_user = 1; // number of cloud users
+		Calendar calendar = Calendar.getInstance();
+		boolean trace_flag = false; // mean trace events
+		
+		// Initialize the CloudSim library
+		CloudSim.init(num_user, calendar, trace_flag);
+	}
+
+	/**
+	 * Creates the broker.
+	 *
+	 * @return the datacenter broker
+	 */
+	private static DatacenterBroker createBroker() {
+		DatacenterBroker broker = null;
+		try {
+			broker = new DatacenterBroker("Broker");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		return broker;
+	}
+	
+}
