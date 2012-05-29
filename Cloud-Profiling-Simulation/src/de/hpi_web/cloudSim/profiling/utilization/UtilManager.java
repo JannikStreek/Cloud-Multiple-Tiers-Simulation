914344312689ed24a645fcddd14f1ba5f9bda3c3
Jannik Streek <jannik@jannik-SATELLITE-R830.(none)>
2012-05-29 19:42:53 +0200

    added a profiling broker, to add cloudlets there

--- Cloud-Profiling-Simulation/src/de/hpi_web/cloudSim/profiling/utilization/UtilManager.java
+++ Cloud-Profiling-Simulation/src/de/hpi_web/cloudSim/profiling/utilization/UtilManager.java
@@ -3,14 +3,23 @@ package de.hpi_web.cloudSim.profiling.utilization;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.cloudbus.cloudsim.Cloudlet;
+import org.cloudbus.cloudsim.Log;
+import org.cloudbus.cloudsim.UtilizationModel;
+import org.cloudbus.cloudsim.UtilizationModelFull;
+import org.cloudbus.cloudsim.core.CloudSim;
 import org.cloudbus.cloudsim.core.CloudSimTags;
 import org.cloudbus.cloudsim.core.SimEntity;
 import org.cloudbus.cloudsim.core.SimEvent;
 
 public class UtilManager extends SimEntity{
 	
-	private static final int RUN = 7000;
-	
+	public static final int RUN = 7000;
+	public static final int CLOUDLET_CREATION = 7001;
+	public static final int ROUND_COMPLETED = 7002;
+	private int brokerId;
+
+
 	//TODO first test with fixed values
     List cpuUtil = new ArrayList<Integer>();
     private int counter;
@@ -39,8 +48,19 @@ public class UtilManager extends SimEntity{
 			case UtilManager.RUN:
 				processRun(ev);
 				break;
+			case UtilManager.ROUND_COMPLETED:
+				processCompleted(ev);
+				break;
+//			case UtilManager.CLOUDLET_CREATION:
+//				processCloudletCreation(ev);
+//				break;
 		}
 				
+	}
+
+	private void processCompleted(SimEvent ev) {
+		Log.printLine(CloudSim.clock() + ": " + getName() + ": Completed Round ");
+		
 	}
 
 	private void processRun(SimEvent ev) {
@@ -49,19 +69,29 @@ public class UtilManager extends SimEntity{
 		
 		//TODO check if enough vms are present / too much vms present and handle this event
 		//schedule ...
-		//TODO create cloudlets for these vms
-		//schedule ...
+		
+		sendNow(brokerId, UtilManager.CLOUDLET_CREATION, cpu);
 		
 		if(counter < cpuUtil.size()) {
 			schedule(getId(), 0, RUN);
 		}
 		
 	}
+	
+
 
 	@Override
 	public void shutdownEntity() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public int getBrokerId() {
+		return brokerId;
+	}
+
+	public void setBrokerId(int brokerId) {
+		this.brokerId = brokerId;
 	}
 
 }
