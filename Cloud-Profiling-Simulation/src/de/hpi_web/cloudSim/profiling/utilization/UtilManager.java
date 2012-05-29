7613c88e3411790b8653f4a522659f066265022e
Jannik Streek <jannik@jannik-SATELLITE-R830.(none)>
2012-05-29 18:30:12 +0200

    added utilmanager

---
+++ Cloud-Profiling-Simulation/src/de/hpi_web/cloudSim/profiling/utilization/UtilManager.java
@@ -0,0 +1,46 @@
+package de.hpi_web.cloudSim.profiling.utilization;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.cloudbus.cloudsim.core.CloudSimTags;
+import org.cloudbus.cloudsim.core.SimEntity;
+import org.cloudbus.cloudsim.core.SimEvent;
+
+public class UtilManager extends SimEntity{
+	
+	private static int RUN = 7000;
+	
+	//TODO first test with fixed values
+    List cpuUtil = new ArrayList<Integer>();
+    
+
+	public UtilManager(String name) {
+		super(name);
+		cpuUtil.add(30);
+		cpuUtil.add(70);
+		cpuUtil.add(130);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void startEntity() {
+		// send the registration to GIS
+		sendNow("utilManager", CloudSimTags.REGISTER_RESOURCE, getId());
+		schedule(getId(), 0, RUN);
+		
+	}
+
+	@Override
+	public void processEvent(SimEvent ev) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void shutdownEntity() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
